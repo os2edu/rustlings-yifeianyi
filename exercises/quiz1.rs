ddebab44eb8f821b101a11d502ea24ae4422a2e2
910002662@qq.com yifeianyi <910002662@qq.com>
2022-11-14 20:28:16 +0800

    2022-11-14 20:28:25

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,19 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(x:i32)->i32{
+    if(x<=40){
+        return x*2;
+    }
+    else{
+        return x;
+    }
+}
 
 // Don't modify this function!
 #[test]
